84a99d3328126840fa23ebd26b77f3eccaf01509
Aven910126 <a1148370488@gmail.com>
2022-02-27 15:50:03 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+<components1></components1>
+</template>
+
+<script setup>
+  import components1 from './components/components1.vue';
+</script>
+
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/components1.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+  <p>資料單向綁定</p>
+  <p>{{ data }}</p>
+
+  <p>資料雙向綁定</p>
+  <input type="text" v-model="text" />
+  <p>{{ text }}</p>
+  </div>
+</template>
+
+<script setup>
+ import { ref } from 'vue'
+const data = "Hello Vue"
+const text =  ref("ss")
+</script>
+
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
